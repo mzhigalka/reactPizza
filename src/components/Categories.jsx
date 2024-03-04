58fbac9c304fdefb6dd53a81397b3749c141910c
Matvey Zhigalka <mzhigalka@gmail.com>
2024-03-04 11:08:53 +0200

    added categeries array

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,13 +1,25 @@
+import React from "react";
+
 export default function Categories() {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+
+  const categories = ["Все", "Мясные", "Вегетарианская", "Гриль", "Острые"];
+
+  const onClickCategory = (index) => {
+    setActiveIndex(index);
+  };
+
   return (
-    <div class="categories">
+    <div className="categories">
       <ul>
-        <li class="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categories.map((value, index) => (
+          <li
+            onClick={() => onClickCategory(index)}
+            className={activeIndex === index ? "active" : ""}
+          >
+            {value}
+          </li>
+        ))}
       </ul>
     </div>
   );
