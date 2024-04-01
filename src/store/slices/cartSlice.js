fffb64963b4d750543dfce1286a26f178b090144
Matvey Zhigalka <mzhigalka@gmail.com>
2024-04-01 20:10:03 +0300

    Added final cart logic

--- src/store/slices/cartSlice.js
+++ src/store/slices/cartSlice.js
@@ -9,12 +9,6 @@ const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
-    // addItems(state, action) {
-    //   state.items.push(action.payload);
-    //   state.totalPrice = state.items.reduce((sum, obj) => {
-    //     return obj.price + sum;
-    //   }, 0);
-    // },
     addItems(state, action) {
       const findItem = state.items.find((obj) => obj.id === action.payload.id);
 
@@ -31,15 +25,24 @@ const cartSlice = createSlice({
         return obj.price * obj.count + sum;
       }, 0);
     },
+    minusItem(state, action) {
+      const findItem = state.items.find((obj) => obj.id === action.payload);
+
+      if (findItem) {
+        findItem.count--;
+      }
+    },
     removeItems(state, action) {
       state.items = state.items.filter((obj) => obj.id !== action.payload);
     },
     clearItems(state) {
       state.items = [];
+      state.totalPrice = 0;
     },
   },
 });
 
-export const { addItems, removeItems, clearItems } = cartSlice.actions;
+export const { addItems, removeItems, minusItem, clearItems } =
+  cartSlice.actions;
 
 export default cartSlice.reducer;
