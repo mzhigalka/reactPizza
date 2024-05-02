6b2578caad5e2152fbb58a2f949a157240f5d2bd
Matvey Zhigalka <mzhigalka@gmail.com>
2024-05-02 20:30:28 +0300

    optimized the assembly - code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,50 @@
 import "./scss/app.scss";
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
-import Cart from "./pages/Cart";
-import NotFound from "./pages/NotFound";
-import FullPizza from "./pages/FullPizza.tsx";
+import Loading from "./components/Loading";
 import MainLayout from "./layouts/MainLayout";
+
+const Cart = React.lazy(
+  () => import(/*webpackChunkName: "Cart"*/ "./pages/Cart")
+);
+const NotFound = React.lazy(
+  () => import(/*webpackChunkName: "NotFound"*/ "./pages/NotFound")
+);
+const FullPizza = React.lazy(
+  () => import(/*webpackChunkName: "FullPizza"*/ "./pages/FullPizza")
+);
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route path="" element={<Home />} />
-        <Route path="cart" element={<Cart />} />
-        <Route path="pizza/:id" element={<FullPizza />} />
-        <Route path="*" element={<NotFound />} />
+        <Route
+          path="cart"
+          element={
+            <React.Suspense fallback={<Loading />}>
+              <Cart />
+            </React.Suspense>
+          }
+        />
+        <Route
+          path="pizza/:id"
+          element={
+            <React.Suspense fallback={<Loading />}>
+              <FullPizza />
+            </React.Suspense>
+          }
+        />
+        <Route
+          path="*"
+          element={
+            <React.Suspense fallback={<Loading />}>
+              <NotFound />
+            </React.Suspense>
+          }
+        />
       </Route>
     </Routes>
   );
